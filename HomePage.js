0f648d6cc9f55d32277b7682cf861742b7315818
s-alkawari <shahadalkawari@gmail.com>
2025-04-14 14:21:35 +0300

    Update HomePage.js

--- HomePage.js
+++ HomePage.js
@@ -1,1 +1,4 @@
-// This is the home page of the application
+// This is the home page of the application which includes three features
+Feature 1: Cool Facts
+Feature 2: Tips for Astronauts
+Feature 3: Countdown for NASA Missions
